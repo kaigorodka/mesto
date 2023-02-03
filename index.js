2e98f4d8d18e029eacee5f54cdbb84e54556ed42
Ivan Kaigorodov <v.k.2001@yandex.ru>
2023-02-04 02:33:35 +0700

    main1

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function openClassRemove() {
   popup.classList.remove("popup_opened");
 }
 closeIcon.addEventListener("click", openClassRemove);
+
+let containerData = document.querySelector(".container-data");
+let popupName = containerData.querySelector(".popup__name");
+let popupStatus = containerData.querySelector(".popup__status");
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  let nameValue = popupName.value;
+  let statusValue = popupStatus.value;
+  let profileName = document.querySelector(".profile__name");
+  let profileStatus = document.querySelector(".profile__status");
+  profileName.textContent = nameValue;
+  profileStatus.textContent = statusValue;
+  openClassRemove();
+}
+containerData.addEventListener("submit", handleFormSubmit);
