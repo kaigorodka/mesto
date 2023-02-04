771bd3d710ead8a303c10b880b3bdd7b356fa42c
Ivan Kaigorodov <v.k.2001@yandex.ru>
2023-02-04 20:56:22 +0700

    main2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let popup = document.querySelector(".popup");
 let editButton = document.querySelector(".edit-button");
 let closeIcon = document.querySelector(".close-icon");
+let profileName = document.querySelector(".profile__name");
+let profileStatus = document.querySelector(".profile__status");
 function openClassAdd() {
   popup.classList.add("popup_opened");
+  profileName.textContent = nameValue;
+  profileStatus.textContent = statusValue;
 }
 editButton.addEventListener("click", openClassAdd);
 
@@ -11,18 +15,16 @@ function openClassRemove() {
 }
 closeIcon.addEventListener("click", openClassRemove);
 
-let containerData = document.querySelector(".container-data");
-let popupName = containerData.querySelector(".popup__name");
-let popupStatus = containerData.querySelector(".popup__status");
+let popupForm = document.querySelector(".popup__form");
+let popupName = popupForm.querySelector(".popup__input_type_name");
+let popupStatus = popupForm.querySelector(".popup__input_type_status");
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   let nameValue = popupName.value;
   let statusValue = popupStatus.value;
-  let profileName = document.querySelector(".profile__name");
-  let profileStatus = document.querySelector(".profile__status");
   profileName.textContent = nameValue;
   profileStatus.textContent = statusValue;
   openClassRemove();
 }
-containerData.addEventListener("submit", handleFormSubmit);
+popupForm.addEventListener("submit", handleFormSubmit);
